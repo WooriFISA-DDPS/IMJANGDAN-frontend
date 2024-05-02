ba9dbf2fa2753eca3c3bdc74fb4d52d95be69e6b
JiJuOh <godinggool@gmail.com>
2024-05-02 10:20:56 +0900

    fix: 깃헙파일 제외

--- src/Components/news/NewsList.js
+++ src/Components/news/NewsList.js
@@ -13,7 +13,8 @@ function NewsList() {
 
   const getNewsList = async (page) => {
     try {
-      const response = await axios.get(`${API_URL}/news/list`, {
+      const response = 
+      await axios.get(`${process.env.REACT_APP_API_URL}/news/list`, {
         params: { page: page - 1 },
       });
 
