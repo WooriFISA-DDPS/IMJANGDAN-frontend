0b0b76440b973a9fff66a72adf0e0565ae7fd6f1
Mugen-Houyou <opnstk@hotmail.com>
2024-05-07 09:28:26 +0900

    feat: 로그인 페이지 기능 구현

--- src/Components/member/LoginNew.js
+++ src/Components/member/LoginNew.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import '../../css/main.css';
+import React, { useContext, useState } from 'react';
+import axios from "axios";
 import styled from 'styled-components';
+import { useNavigate } from 'react-router';
+
+import '../../css/main.css';
+import { AuthContext } from '../context/AuthProvider';
+import { HttpHeadersContext } from '../context/HttpHeadersProvider';
+import { Link } from 'react-router-dom';
 
 const ColoredBgDiv = styled.div`
     --tw-bg-opacity: 1;
@@ -42,29 +48,84 @@ const TitleImageDiv = styled.div`
     text-align: center;
     width: 400px;
     background-image: url('/images/IMJANGDAN.png');
-    z-index: 999999999;
+    z-index: -1;
     height: 300px;
     background-size: contain;
     background-repeat: no-repeat;
+    
 }
 `;
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const { auth, setAuth } = useContext(AuthContext);
+  const { headers, setHeaders } = useContext(HttpHeadersContext);
+
+  const changeId = (event) => {
+    setId(event.target.value);
+  }
+
+  const changePwd = (event) => {
+    setPwd(event.target.value);
+  }
+
+  const gotoJoin = (event) => {
+    navigate('/join');
+  }
+
+  const reqLogin = async () => {
+
+    const req = {
+      email: id,
+      password: pwd
+    }
+
+    console.log("#############", id, pwd)
+
+    await axios.post(`${process.env.REACT_APP_API_URL}/user/login`, req)
+      .then((resp) => {
+        console.log("[Login.js] login() success :D");
+        console.log(resp.data);
+
+        alert(resp.data.email + ", successfully signed in! 🔐");
+
+        // JWT 토큰 저장
+        localStorage.setItem("bbs_access_token", resp.data.token);
+        localStorage.setItem("id", resp.data.email);
+
+        setAuth(resp.data.email); // 사용자 인증 정보(아이디 저장)
+        setHeaders({ "Authorization": `Bearer ${resp.data.toekn}` }); // 헤더 Authorization 필드 저장
+
+        navigate("/homememo");
+
+
+      }).catch((err) => {
+        console.log("[Login.js] login() error :<");
+        console.log(err);
+
+        alert("⚠️ is it here? 3 " + err.response.data);
+      });
+  }
+
   return (<>
     <ColoredBgDiv>
-    <TitleImageDiv></TitleImageDiv>
-    <TitleDiv>임장단: 위치기반 개인 부동산 임장지도 커뮤니케이션 플랫폼</TitleDiv>
+      <TitleImageDiv></TitleImageDiv>
+      <TitleDiv>임장단: 위치기반 개인 부동산 임장지도 커뮤니케이션 플랫폼</TitleDiv>
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full" style={{ position: 'relative', zIndex: 1 }}>
-        <h2 className="text-lg font-semibold mb-4">로그인하고 서비스를 이용하세요</h2>
-        <form>
+        <h2 className="text-lg font-semibold mb-4">로그인하고 서비스를 이용하세요!</h2>
+        
           <div className="mb-4">
-            <input type="email" placeholder="Email" className="w-full p-2 border border-gray-300 rounded" />
+            <input type="email" value={id} onChange={changeId} placeholder="Email" className="w-full p-2 border border-gray-300 rounded" />
           </div>
           <div className="mb-6">
-            <input type="password" placeholder="Password" className="w-full p-2 border border-gray-300 rounded" />
+            <input type="password" value={pwd} onChange={changePwd} placeholder="Password" class="w-full p-2 border border-gray-300 rounded" />
           </div>
-          <button type="submit" className="w-full bg-black text-white p-3 rounded">로그인하기</button>
-        </form>
+          <button type="submit" onClick={reqLogin} className="w-full bg-black text-white p-3 rounded">로그인</button>
+        
         <div className="flex justify-center space-x-2 mt-4">
           <button className="bg-yellow-400 rounded-full w-10 h-10"></button>
           <button className="bg-blue-500 rounded-full w-10 h-10"></button>
@@ -72,13 +133,13 @@ function Login() {
           <button className="bg-red-600 rounded-full w-10 h-10"></button>
         </div>
         <div className="mt-4">
-          <button className="w-full mt-2 bg-blue-500 text-white p-2 rounded">회원가입</button>
+          <button className="w-full mt-2 bg-blue-500 text-white p-2 rounded" onClick={gotoJoin} >회원가입</button>
           <div className="flex justify-center space-x-2 mt-4"><a href='#'>아이디/비밀번호 찾기</a></div>
         </div>
       </div>
       <RightCornerDiv />
     </ColoredBgDiv>
-    </>
+  </>
   );
 }
 
