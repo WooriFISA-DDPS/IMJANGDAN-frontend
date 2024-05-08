635d817686d888034bfb2e69f66134f182b3e2b8
JiJuOh <godinggool@gmail.com>
2024-05-08 11:26:42 +0900

    fix: /링크 타임리프로 이동

--- src/Components/router/Router.js
+++ src/Components/router/Router.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
 
-import KakaoMapTest from '../app/KakaoMapTest'
 import BbsList from "../bbs/BbsList"
 import BbsWrite from "../bbs/BbsWrite"
 import BbsDetail from "../bbs/BbsDetail"
@@ -10,7 +9,7 @@ import BbsAnswer from "../bbs/BbsAnswer"
 import NewsList from "../news/NewsList";
 import Join from "../member/Join"
 import Login from "../member/Login"
-import LoginNew from "../member/LoginV2"
+// import LoginNew from "../member/LoginV2"
 import Logout from "../member/Logout"
 import MemberUpdate from "../member/MemberUpdate";
 import CheckPwd from "../member/CheckPwd";
@@ -23,59 +22,65 @@ import LoginV2 from "../member/LoginV2";
 
 function Router() {
 
+
 	function useIsMobile() {
 		const [isMobile, setIsMobile] = useState(false);
-	
+
 		useEffect(() => {
 			const handleResize = () => {
 				const width = window.innerWidth;
 				setIsMobile(width <= 645); // Adjust the threshold as needed (common breakpoint for mobile)
 			};
-	
+
 			window.addEventListener('resize', handleResize);
-	
+
 			handleResize(); // Call on initial render
-	
+
 			return () => window.removeEventListener('resize', handleResize);
 		}, []);
-	
+
 		return isMobile;
 	}
 
 	const isMobile = useIsMobile();
 
+	const LeadToMain = () => {
+		window.location.href =`${process.env.REACT_APP_API_URL}/memomap`;
+		return null;
+	}
+
 	return (
-			<Routes>
-				<Route path="/" element={<KakaoMapTest />}></Route>
-				<Route path="/bbslist" element={<DefaultLayout><BbsList /></DefaultLayout>}></Route>
-				<Route path="/bbswrite" element={<DefaultLayout><BbsWrite /></DefaultLayout>}></Route>
-				<Route path="/bbsdetail/:boardId" element={<DefaultLayout><BbsDetail /></DefaultLayout>}></Route>
-				<Route path="/bbsupdate" element={<DefaultLayout><BbsUpdate /></DefaultLayout>}></Route>
-				<Route path="/bbsanswer/:parentSeq" element={<DefaultLayout><BbsAnswer /></DefaultLayout>}></Route>
+		<Routes>
+			<Route path="/" element={<LeadToMain />} />
+			<Route path="/bbslist" element={<DefaultLayout><BbsList /></DefaultLayout>}></Route>
+			<Route path="/bbswrite" element={<DefaultLayout><BbsWrite /></DefaultLayout>}></Route>
+			<Route path="/bbsdetail/:boardId" element={<DefaultLayout><BbsDetail /></DefaultLayout>}></Route>
+			<Route path="/bbsupdate" element={<DefaultLayout><BbsUpdate /></DefaultLayout>}></Route>
+			<Route path="/bbsanswer/:parentSeq" element={<DefaultLayout><BbsAnswer /></DefaultLayout>}></Route>
 
-				<Route
-          path="/homememo"
-          element={
-            !isMobile ? (
-              <DefaultLayout>
-                <HomeMemo />
-              </DefaultLayout>
-            ) : (
-              <HomeMemoMobile />
-            )
-          }
-        />
+			<Route
+				path="/homememo"
+				element={
+					!isMobile ? (
+						<DefaultLayout>
+							<HomeMemo />
+						</DefaultLayout>
+					) : (
+						<HomeMemoMobile />
+					)
+				}
+			/>
 
-				<Route path="/newslist" element={<DefaultLayout><NewsList /></DefaultLayout>}></Route>
+			<Route path="/newslist" element={<DefaultLayout><NewsList /></DefaultLayout>}></Route>
 
-				<Route path="/loginold" element={<DefaultLayout><Login /></DefaultLayout>}></Route>
-				<Route path="/login" element={<LoginV2 />}></Route>
-				<Route path="/join" element={<DefaultLayout><Join /></DefaultLayout>}></Route>
-				<Route path="/checkpwd" element={<DefaultLayout><CheckPwd /></DefaultLayout>}></Route>
-				<Route path="/update" element={<DefaultLayout><MemberUpdate /></DefaultLayout>}></Route>
-				<Route path="/adminfeat" element={<DefaultLayout><AdminList /></DefaultLayout>}></Route>
-				<Route path="/logout" element={<Logout />}></Route>
-			</Routes>
+			<Route path="/loginold" element={<DefaultLayout><Login /></DefaultLayout>}></Route>
+			<Route path="/login" element={<LoginV2 />}></Route>
+			<Route path="/join" element={<DefaultLayout><Join /></DefaultLayout>}></Route>
+			<Route path="/checkpwd" element={<DefaultLayout><CheckPwd /></DefaultLayout>}></Route>
+			<Route path="/update" element={<DefaultLayout><MemberUpdate /></DefaultLayout>}></Route>
+			<Route path="/adminfeat" element={<DefaultLayout><AdminList /></DefaultLayout>}></Route>
+			<Route path="/logout" element={<Logout />}></Route>
+		</Routes>
 	);
 }
 
